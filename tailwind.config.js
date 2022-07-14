2d5a0dd42e4b287b76fc08be49e24ec5c8ffffdc
RenrevZ <harvy275@gmail.com>
2022-07-14 23:26:55 +0800

    added a configuration typography

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./app/views/pages/*.php'],
+  theme: {
+    screens:{
+      sm : '480px',
+      md : '768px',
+      lg : '1024px',
+      xl : '1280px'
+    },
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    // ...
+  ],
+}
