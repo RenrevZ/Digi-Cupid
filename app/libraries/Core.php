459272503a91de9ca456161f8122417774455a71
RenrevZ <harvy275@gmail.com>
2022-07-14 21:53:36 +0800

    initial commit

---
+++ app/libraries/Core.php
@@ -0,0 +1,76 @@
+<?php
+   /* 
+    - App core Class
+    - Creates a URL to load in Controller
+   */
+
+   class Core {
+        protected $currentController = 'Pages';
+        protected $currentMethod = 'index';
+        protected $params = [];
+
+        public function __construct()
+        {
+            $url = $this->getURL();
+
+            //find the first value in the controller/ url
+            if($url && file_exists('../app/controllers/'.ucwords($url[0]).'.php')){
+                // if the request url exist then set as controller
+                $this->currentController = ucwords($url[0]);
+
+                // uset the 0 index
+                unset($url[0]);
+            }
+
+            // Require the controller
+            require_once '../app/controllers/'. $this->currentController.'.php';
+
+            // instantiate controller class
+            $this->currentController = new $this->currentController;
+
+            // check for second url/ method in url
+            if(isset($url[1])){
+                //check if method exist into controller
+                if(method_exists($this->currentController,$url[1])){
+                    $this->currentMethod = $url[1];
+                }
+            }
+
+            // check for second url/ method in url
+            if(isset($url[1])){
+                //check if method exist into controller
+                if($url && method_exists($this->currentController,$url[1])){
+                    $this->currentMethod = $url[1];
+
+                    unset($url[1]);
+                }
+            }
+
+
+            // get params
+            $this->params = $url ? array_values($url) : [];
+
+            // callback with the array of params
+            call_user_func_array([$this->currentController,
+                $this->currentMethod],$this->params);
+
+
+        }
+
+       public function getURL()
+       {
+           if(isset($_GET['url'])){
+               // breaking the url using '/'
+               $url = rtrim($_GET['url'], '/');
+
+               //filter the url
+               $url = filter_var($url,FILTER_SANITIZE_URL);
+
+               // break the url into a array
+               $url = explode('/',$url);
+
+               return $url;
+           }
+       }
+   }
+
---
+++ public/app/config/config.php
@@ -0,0 +1,17 @@
+<?php
+    /*
+     * Defining all constant in the database
+     * Data that has been loaded by Database controller-
+     * was fetch and coming through here
+     */
+    // DB Params
+    define('DB_HOST','localhost');
+    define('DB_USER','root');
+    define('DB_PASS','');
+    define('DB_NAME','Travel Share');
+    // app root
+    define('APPROOT',dirname(dirname(__FILE__)));
+    // url root
+    define('URLROOT','http://localhost/HG-FRAME-MVC');// insert your localhost url here
+    // appname
+    define('APPNAME','HG-FRAME-MVC');
